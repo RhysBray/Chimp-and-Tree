04dbbf4aa0fe5366dd96186d29110a7fa2e79c31
Chloe Hewer <chloe.hewer@Nology.local>
2019-04-21 13:18:28 +0100

    trying

--- src/components/monkey/monkey.tsx
+++ src/components/monkey/monkey.tsx
@@ -4,16 +4,23 @@ import monkey from "../../imgs/monkey.png";
 
 export interface IProps {}
 
-export interface IState {}
+export interface IState {
+  flippedMonkey: any;
+  isFlipped: boolean;
+}
 
 class Monkey extends React.Component<IProps, IState> {
+  public state = { flippedMonkey: styles.monkeyLeft, isFlipped: false };
+
   public render() {
     return (
       <div className={styles.monkeyFrame}>
-        <img className={styles.monkey} src={monkey} />
+        <img
+          className={`${styles.monkey} ${this.state.flippedMonkey}`}
+          src={monkey}
+        />
       </div>
     );
   }
 }
-
 export default Monkey;
--- src/containers/game-play.tsx
+++ src/containers/game-play.tsx
@@ -2,60 +2,40 @@ import * as React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import styles from "./game-play.module.scss";
 import Branch from "../components/tree-branches/treeBranches";
-import LeftBranch from "../imgs/leftBranch.png";
-import RightBranch from "../imgs/rightBranch.png";
-import BothBranches from "../imgs/bothBranches.png";
+import leftBranch from "../imgs/leftBranch.png";
+import rightBranch from "../imgs/rightBranch.png";
+import bothBranches from "../imgs/bothBranches.png";
+import bottomBranch from "../imgs/bottomBranch.png";
 import Monkey from "../components/monkey/monkey";
 
 export interface IProps {}
 
 export interface IState {
   branch: string;
+  branchArray: string[];
 }
 
 class PlayGame extends React.Component<IProps, IState> {
-  public state = { branch: "" };
+  public state = { branch: "", branchArray: [""] };
+  public branchArray = [leftBranch, rightBranch, bothBranches, bottomBranch];
 
-  public branchRand() {
-    let random = Math.floor(Math.random() * 3 + 1);
-
-    // switch (random) {
-    //   case 1:
-    //     this.state.branch = LeftBranch;
-    //     return;
-    //   case 2:
-    //     this.state.branch = RightBranch;
-    //     return;
-    //   case 3:
-    //     this.state.branch = BothBranches;
-    //     return;
-    // }
-
-    if (random === 1) {
-      this.state.branch = LeftBranch;
-    } else if (random === 2) {
-      this.state.branch = RightBranch;
-    } else {
-      this.state.branch = BothBranches;
-    }
-  }
+  // public branchRand() {
+  //   let random = Math.floor(Math.random() * 3 + 1);
+  //   if (random === 1) {
+  //     this.state.branch = leftBranch;
+  //   } else if (random === 2) {
+  //     this.state.branch = rightBranch;
+  //   } else {
+  //     this.state.branch = bothBranches;
+  //   }
+  // }
 
   public render() {
-    let branchArray = ["branch", "branch"];
     return (
       <div className={styles.gameContainer}>
-        {this.branchRand()}
-        <Branch branch={this.state.branch} />
-        {this.branchRand()}
-        <Branch branch={this.state.branch} />
-        {this.branchRand()}
-        <Branch branch={this.state.branch} />
-        {this.branchRand()}
-        <Branch branch={this.state.branch} />
-        {this.branchRand()}
-        <Branch branch={this.state.branch} />
-        {this.branchRand()}
-        <Branch branch={this.state.branch} />
+        {this.branchArray.map(index => {
+          return <Branch branch={index} />;
+        })}
         <Monkey />
       </div>
     );
